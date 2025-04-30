9e2a4002ccac87b7ffbc954a0d42038643003520
Aman Somani <70381713+Aman235820@users.noreply.github.com>
2025-04-30 23:14:19 +0530

    Add files via upload

---
+++ src/main/java/com/example/labourApp/LabourAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.labourApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabourAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabourAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/labourApp/LabourAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labourApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabourAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
